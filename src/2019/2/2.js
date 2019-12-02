baa23f6230f496379ab738ca906072527e489f10
Graziano Statello <grazianos86@gmail.com>
2019-12-02 14:02:39 +0000

    day 2, part 2

--- src/2019/2/2.js
+++ src/2019/2/2.js
@@ -1,22 +1,39 @@
-export const part1 = (values) => {
+const intCode = ({ inputs, noun, verb}) => {
+  const values = inputs.map((val, i) => (i === 1 && noun) || (i === 2 && verb) || val)
   let index = 0
   let halt = false
 
   while (!halt) {
     const opCode = values[index]
-    const inputA = values[index + 1]
-    const inputB = values[index + 2]
-    const storeIndex = values[index + 3]
-    if (opCode === 1) {
-      values[storeIndex] = values[inputA] + values[inputB]
-    } else if (opCode === 2) {
-      values[storeIndex] = values[inputA] * values[inputB]
-    } else if (opCode === 99) {
-      halt = true
+    if (opCode === 99) halt = true
+    else {
+      const parameters = [values[index + 1], values[index + 2], values[index + 3]]
+      if (opCode === 1) {
+        values[parameters[2]] = values[parameters[0]] + values[parameters[1]]
+      } else if (opCode === 2) {
+        values[parameters[2]] = values[parameters[0]] * values[parameters[1]]
+      }
     }
-
     index += 4
     if (index >= values.length - 1) halt = true
   }
   return values[0]
+}
+
+export const part1 = (inputs) => {
+  return intCode({ inputs, noun: 12, verb: 2 })
+}
+
+export const part2 = (inputs) => {
+  let res = 0
+  for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+      const calc = intCode({ inputs, noun, verb })
+      if (calc === 19690720) {
+        res = `${noun}${verb}`
+        break
+      }
+    }
+  }
+  return parseInt(res)
 }
--- src/2019/2/2.test.js
+++ src/2019/2/2.test.js
@@ -1,13 +1,13 @@
-import { part1 } from './2'
+import { part1, part2 } from './2'
 import input from './input.js'
 
 describe('Day 2: 1202 Program Alarm', () => {
-  it('Part 1, test values', () => {
-    const values = [1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50]
-    expect(part1(values)).toEqual(3500)
-  })
   it('Part 1', () => {
     const values = input.split(',').map(i => parseInt(i))
     expect(part1(values)).toEqual(4138658)
+  })
+  it('Part 2', () => {
+    const values = input.split(',').map(i => parseInt(i))
+    expect(part2(values)).toEqual(7264)
   })
 })
