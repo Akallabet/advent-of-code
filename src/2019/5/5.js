5a682a63cf8fcf29def71fb338a86b84aff0e339
Graziano Statello <grazianos86@gmail.com>
2019-12-09 16:26:31 +0000

    day 5 part 2

--- src/2019/5/5.js
+++ src/2019/5/5.js
@@ -1,7 +1,3 @@
-const getNumOfInstructions = opcode => {
-  return (opcode === 1 || opcode === 2) ? 4 : 2
-}
-
 export const part1 = (inputValues, input) => {
   const values = inputValues.split(',').map(i => parseInt(i))
   const outputs = []
@@ -9,6 +5,7 @@ export const part1 = (inputValues, input) => {
   let halt = false
 
   const useMode = (mode = 0, par) => parseInt(mode) ? par : values[par]
+  const getNumOfInstructions = opcode => (opcode === 1 || opcode === 2) ? 4 : 2
 
   while (!halt) {
     const instruction = values[pos]
@@ -27,4 +24,47 @@ export const part1 = (inputValues, input) => {
   return outputs[outputs.length - 1]
 }
 
-export const part2 = () => {}
+export const part2 = (inputValues, input) => {
+  const values = inputValues.split(',').map(i => parseInt(i))
+  const outputs = []
+  let pos = 0
+  let halt = false
+
+  const useMode = (mode = 0, par) => parseInt(mode) ? parseInt(par) : parseInt(values[par])
+  const getNumOfInstructions = opcode => (opcode === 3 || opcode === 4) ? 2 : 4
+
+  while (!halt) {
+    const instruction = values[pos]
+    const [opcodeStr,, mode1, mode2] = `${instruction}`.split('').reverse()
+    const opcode = parseInt(opcodeStr)
+    const numOfInstructions = getNumOfInstructions(opcode)
+    const [par1, par2, par3] = values.slice(pos + 1, pos + numOfInstructions)
+    if (opcode === 9 || pos >= values.length) halt = true
+    else if (opcode === 1) {
+      values[par3] = useMode(mode1, par1) + useMode(mode2, par2)
+      pos += 4
+    } else if (opcode === 2) {
+      values[par3] = useMode(mode1, par1) * useMode(mode2, par2)
+      pos += 4
+    } else if (opcode === 3) {
+      values[par1] = input
+      pos += 2
+    } else if (opcode === 4) {
+      outputs.push(useMode(mode1, par1))
+      pos += 2
+    } else if (opcode === 5) {
+      if (useMode(mode1, par1)) pos = useMode(mode2, par2)
+      else pos += 3
+    } else if (opcode === 6) {
+      if (!useMode(mode1, par1)) pos = useMode(mode2, par2)
+      else pos += 3
+    } else if (opcode === 7) {
+      values[par3] = useMode(mode1, par1) < useMode(mode2, par2) ? 1 : 0
+      pos += 4
+    } else if (opcode === 8) {
+      values[par3] = useMode(mode1, par1) === useMode(mode2, par2) ? 1 : 0
+      pos += 4
+    }
+  }
+  return outputs[outputs.length - 1]
+}
--- src/2019/5/5.test.js
+++ src/2019/5/5.test.js
@@ -9,8 +9,8 @@ describe('Day 5: Sunny with a Chance of Asteroids', () => {
   })
 
   it('Part 2', () => {
-    const training = '3,0,4,0,99'
-    expect(part2(training, 2)).toEqual(2)
-    expect(part2(input, 1)).toEqual(12440243)
+    const training = '3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99'
+    expect(part2(training, 9)).toEqual(1001)
+    expect(part2(input, 5)).toEqual(15486302)
   })
 })
