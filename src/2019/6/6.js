d99c36957cd996399ec57523928de627266a93dd
Graziano Statello <grazianos86@gmail.com>
2019-12-10 14:34:38 +0000

    day 6 part 2

--- src/2019/6/6.js
+++ src/2019/6/6.js
@@ -1,8 +1,15 @@
 const buildOrbitMap = planets => planets
   .reduce((orbitMap, orbit) => {
     const [planet, satellite] = orbit.split(')')
-    if (!orbitMap.get(planet)) orbitMap.set(planet, [])
-    orbitMap.get(planet).push(satellite)
+    if (!orbitMap.get(planet)) {
+      orbitMap.set(planet, {
+        satellites: []
+      })
+    }
+    if (!orbitMap.get(satellite)) {
+      orbitMap.set(satellite, { satellites: [] })
+    }
+    orbitMap.get(planet).satellites.push(satellite)
     return orbitMap
   }, new Map())
 
@@ -10,11 +17,10 @@ export const part1 = (inputValues) => {
   const orbitMap = buildOrbitMap(inputValues.split('\n'))
   let totalOrbits = 0
   let orbitDeep = 0
-
   const deepSearch = (planet) => {
-    const satellites = orbitMap.get(planet)
+    const { satellites } = orbitMap.get(planet)
     totalOrbits += orbitDeep
-    if (!satellites) {
+    if (!satellites.length) {
       orbitDeep--
       return
     }
@@ -27,4 +33,39 @@ export const part1 = (inputValues) => {
 
   deepSearch('COM')
   return totalOrbits
+}
+
+export const part2 = (inputValues) => {
+  const orbitMap = buildOrbitMap(inputValues.split('\n'))
+  const addParentPlanets = (planet, parent) => {
+    const system = orbitMap.get(planet)
+    if (!system) return
+    system.parent = parent
+    const { satellites } = system
+    for (let i = 0; i < satellites.length; i++) {
+      addParentPlanets(satellites[i], planet)
+    }
+  }
+
+  const getParents = (planet, hops = 0, parents = new Map()) => {
+    const { parent } = orbitMap.get(planet)
+    if (!parent) {
+      return parents
+    }
+    parents.set(planet, hops)
+    return getParents(parent, hops + 1, parents)
+  }
+
+  addParentPlanets('COM')
+  const youParents = getParents('YOU')
+  const sanParents = getParents('SAN')
+  let minHops = 0
+  for (const parent of youParents.keys()) {
+    const sanParent = sanParents.get(parent)
+    if (sanParent) {
+      const hops = sanParent + youParents.get(parent)
+      minHops = (!minHops && hops) || (hops < minHops && hops) || minHops
+    }
+  }
+  return minHops - 2
 }
--- src/2019/6/6.test.js
+++ src/2019/6/6.test.js
@@ -1,7 +1,7 @@
-import { part1 } from './6'
+import { part1, part2 } from './6'
 import input from './input'
 
-const training = `COM)B
+const training1 = `COM)B
 B)C
 C)D
 D)E
@@ -13,9 +13,28 @@ E)J
 J)K
 K)L`
 
+const training2 = `COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN`
+
 describe('Day 6: Universal Orbit Map', () => {
   it('Part 1', () => {
-    expect(part1(training)).toEqual(42)
+    expect(part1(training1)).toEqual(42)
     expect(part1(input)).toEqual(315757)
+  })
+
+  it('Part 2', () => {
+    expect(part2(training2)).toEqual(4)
+    expect(part2(input)).toEqual(481)
   })
 })
