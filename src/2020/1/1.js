94c25bece56fa48e468f5c6a1d01559162e5610a
Akallabet <grazianos86@gmail.com>
2020-12-01 15:23:48 +0000

    2020, day 1, part 2

--- src/2020/1/1.js
+++ src/2020/1/1.js
@@ -1,13 +1,30 @@
-
-export const part1 = (values) => {
+const searchForTotal = ({ values, total }) => {
   let result
 
   for (let i = 0; i < values.length; i++) {
-    const match = values[values.indexOf(2020 - values[i])]
+    const match = values[values.indexOf(total - values[i])]
     if (match) {
       result = values[i] * match
       break
     }
   }
+  return result
+}
+
+export const part1 = values => {
+  return searchForTotal({ values, total: 2020 })
+}
+
+export const part2 = values => {
+  let result
+
+  for (let i = 0; i < values.length; i++) {
+    const match = searchForTotal({ values, total: 2020 - values[i] })
+    if (match) {
+      result = values[i] * match
+      break
+    }
+  }
+
   return result
 }
--- src/2020/1/1.test.js
+++ src/2020/1/1.test.js
@@ -1,19 +1,27 @@
-import { part1 } from './1'
+import { part1, part2 } from './1'
 import input from './input.js'
 
-describe('Day 1: Report Repair', () => {
-  it('Part 1 - Example', () => {
-    const example = `1721
+const example = `1721
     979
     366
     299
     675
     1456`
-    // const values = input.split('\n').map(i => parseInt(i))
+
+describe('Day 1: Report Repair', () => {
+  it('Part 1 - Example', () => {
     expect(part1(example.split('\n').map(i => parseInt(i)))).toEqual(514579)
   })
 
   it('Part 1', () => {
     expect(part1(input.split('\n').map(i => parseInt(i)))).toEqual(440979)
+  })
+
+  it('Part 2 - Example', () => {
+    expect(part2(example.split('\n').map(i => parseInt(i)))).toEqual(241861950)
+  })
+
+  it('Part 2', () => {
+    expect(part2(input.split('\n').map(i => parseInt(i)))).toEqual(82498112)
   })
 })
