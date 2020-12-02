bfba454ab8bbdd0db43f1b93cd988a60cc425a1b
Akallabet <grazianos86@gmail.com>
2020-12-02 10:35:32 +0000

    2020, day 2, part 2

--- src/2020/2/2.js
+++ src/2020/2/2.js
@@ -1,22 +1,29 @@
-export const part1 = values => {
-  return values.map(row => {
-    const [policy, password] = row.split(': ')
-    const [frequency, letter] = policy.split(' ')
-    const [min, max] = frequency.split('-').map(Number)
-    return {
-      min,
-      max,
-      letter,
-      password
-    }
-  }).reduce((valid, {
+
+const normalizeValue = row => {
+  const [policy, password] = row.split(': ')
+  const [frequency, letter] = policy.split(' ')
+  const [min, max] = frequency.split('-').map(Number)
+  return {
     min,
     max,
     letter,
     password
-  }) => {
-    const difference = password.length - password.split(letter).join('').length
-    if (difference >= min && difference <= max) valid += 1
-    return valid
-  }, 0)
+  }
 }
+
+export const part1 = values => values.map(normalizeValue).reduce((valid, {
+  min,
+  max,
+  letter,
+  password
+}) => {
+  const difference = password.length - password.split(letter).join('').length
+  return (difference >= min && difference <= max) ? valid + 1 : valid
+}, 0)
+
+export const part2 = values => values.map(normalizeValue).reduce((valid, {
+  min: first,
+  max: second,
+  letter,
+  password
+}) => (password[first - 1] === letter) ^ (password[second - 1] === letter) ? valid + 1 : valid, 0)
--- src/2020/2/2.test.js
+++ src/2020/2/2.test.js
@@ -1,4 +1,4 @@
-import { part1 } from './2'
+import { part1, part2 } from './2'
 import input from './input.js'
 
 const example = `1-3 a: abcde
@@ -12,5 +12,13 @@ describe('Day 2: Password Philosophy', () => {
 
   it('Part 1', () => {
     expect(part1(input.split('\n'))).toEqual(600)
+  })
+
+  it('Part 2 - Example', () => {
+    expect(part2(example.split('\n'))).toEqual(1)
+  })
+
+  it('Part 2', () => {
+    expect(part2(input.split('\n'))).toEqual(245)
   })
 })
