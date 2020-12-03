4aab32c491693c5941c8c648488b9201e1245088
Akallabet <grazianos86@gmail.com>
2020-12-03 09:24:37 +0000

    2020, day 3, part 2

--- src/2020/3/3.js
+++ src/2020/3/3.js
@@ -1,8 +1,16 @@
-export const part1 = slopes => {
-  let right = 0
-  return slopes.slice(1, slopes.length).reduce((trees, slope) => {
-    const turn = right + 3
-    right = turn >= slope.length ? turn - slope.length : turn
-    return slope[right] === '#' ? trees + 1 : trees
-  }, 0)
+const run = rows => ([down, right]) => {
+  let slope = 0
+  let trees = 0
+  for (let i = down; i < rows.length; i += down) {
+    slope = slope + right
+    if (!rows[i][slope]) slope = slope - rows[i].length
+    if (rows[i][slope] === '#') trees++
+  }
+  return trees
+}
+
+export const part1 = rows => run(rows)([1, 3])
+
+export const part2 = rows => {
+  return [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]].reduce((trees, increment) => trees * run(rows)(increment), 1)
 }
--- src/2020/3/3.test.js
+++ src/2020/3/3.test.js
@@ -1,4 +1,4 @@
-import { part1 } from './3'
+import { part1, part2 } from './3'
 import input from './input.js'
 
 const example = `..##.......
@@ -19,5 +19,11 @@ describe('Day 3: Toboggan Trajectory', () => {
   })
   it('Part 1', () => {
     expect(part1(input.split('\n'))).toEqual(276)
+  })
+  it('Part 2 - Example', () => {
+    expect(part2(example.split('\n'))).toEqual(336)
+  })
+  it('Part 2', () => {
+    expect(part2(input.split('\n'))).toEqual(7812180000)
   })
 })
