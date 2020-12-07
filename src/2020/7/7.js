7da224913445e647a9ae603e94c5b997442a7ea3
Akallabet <grazianos86@gmail.com>
2020-12-07 08:20:38 +0000

    2020, day 7, part 1

--- src/2020/7/7.js
+++ src/2020/7/7.js
@@ -1,3 +1,19 @@
-export const part1 = values => {}
+const colorsContainingBag = (bags, colors = {}) => bag => {
+  bags
+    .filter(line => line.indexOf(bag) > 0)
+    .map(line => {
+      const [color] = line.split(' bags contain ')
+      colors[color] = 1
+      return color
+    })
+    .forEach(colorsContainingBag(bags, colors))
+
+  return colors
+}
+
+export const part1 = values => {
+  const totalColors = colorsContainingBag(values, [])('shiny gold')
+  return Object.keys(totalColors).length
+}
 
 export const part2 = values => {}
--- src/2020/7/7.test.js
+++ src/2020/7/7.test.js
@@ -1,8 +1,21 @@
-// import { part1, part2 } from './7'
-// import input from './input'
+import { part1 } from './7'
+import input from './input'
 
-// const example = ''
+const example = `light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`
 
-describe('Day 7: ???', () => {
-  it.todo('Part 1 - Example')
+describe('Day 7: Handy Haversacks', () => {
+  it('Part 1 - Example', () => {
+    expect(part1(example.split('\n'))).toEqual(4)
+  })
+  it('Part 1', () => {
+    expect(part1(input.split('\n'))).toEqual(112)
+  })
 })
