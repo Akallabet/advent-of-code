424184be190df21a14f1721b9c897491971e1abe
Akallabet <grazianos86@gmail.com>
2020-12-08 08:28:07 +0000

    improve solution

--- src/2020/8/8.js
+++ src/2020/8/8.js
@@ -2,64 +2,40 @@ const makeInstructions = values => values.map((instr, index) => {
   const [command, value] = instr.split(' ')
   return {
     command,
-    value,
+    value: Number(value),
     index
   }
 })
 
-const startExecute = instructions => {
-  let accumulator = 0
-  const visited = {}
-  let lastIndex = 0
+const execute = (instructions, index = 0, accumulator = 0, visited = {}) =>
+  (!instructions[index] || visited[index])
+    ? { accumulator, index }
+    : execute(
+      instructions,
+      (instructions[index].command === 'jmp') ? index + instructions[index].value : index + 1,
+      accumulator + ((instructions[index].command === 'acc') ? instructions[index].value : 0),
+      { ...visited, [index]: 1 }
+    )
 
-  const execute = instruction => {
-    if (!instruction) return
-    const { command, value, index } = instruction
-    if (visited[index]) return
-    switch (command) {
-      case 'nop':
-        visited[index] = 1
-        lastIndex = index + 1
-        execute(instructions[index + 1])
-        break
-      case 'acc':
-        visited[index] = 1
-        accumulator += Number(value)
-        lastIndex = index + 1
-        execute(instructions[index + 1])
-        break
-      case 'jmp':
-        visited[index] = 1
-        lastIndex = index + Number(value)
-        execute(instructions[index + Number(value)])
-        break
-    }
-  }
-  execute(instructions[0])
-  return { accumulator, lastIndex }
-}
-
-export const part1 = values => {
-  const instructions = makeInstructions(values)
-  return startExecute(instructions).accumulator
-}
+export const part1 = values => execute(makeInstructions(values)).accumulator
 
 export const part2 = values => {
   const instructions = makeInstructions(values)
 
-  const instructionsToChange = instructions.filter(({ command }) => command !== 'acc').map(({ command, ...instr }) => {
+  const instructionsToChange = instructions.filter(({ command }) => command !== 'acc').map(({ command, value, index }) => {
     return {
       command: command === 'jmp' ? 'nop' : 'jmp',
-      ...instr
+      value,
+      index
     }
   })
 
   for (let i = 0; i < instructionsToChange.length; i++) {
     const instrToChange = instructionsToChange[i]
-    const { lastIndex, accumulator } = startExecute(instructions.map((instr, index) => {
+    const { index, accumulator } = execute(instructions.map((instr, index) => {
       return index === instrToChange.index ? instrToChange : instr
     }))
-    if (lastIndex >= instructions.length) return accumulator
+    if (index >= instructions.length) return accumulator
   }
   return ''
 }
