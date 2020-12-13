492b1596a935ec784560f32e355b715c764ed91e
Akallabet <grazianos86@gmail.com>
2020-12-13 22:45:30 +0000

    2020, day 13, part 2

--- src/2020/13/13.js
+++ src/2020/13/13.js
@@ -9,4 +9,22 @@ export const part1 = ([departure, times]) => {
   }, { id: '', timestamp: departure })
   return id * timestamp
 }
-export const part2 = (values) => {}
+
+export const part2 = ([_, times]) => {
+  return times
+    .split(',')
+    .map((time, offset) => ({ time, offset }))
+    .filter(({ time }) => time !== 'x')
+    .map(({ time, offset }) => ({ time: Number(time), offset }))
+    .reduce(({ solution, multi }, { time, offset }) => {
+      while (true) {
+        if ((solution + offset) % time === 0) {
+          multi *= time
+          break
+        }
+        solution += multi
+      }
+      // console.log(solution, time, offset)
+      return { solution, multi }
+    }, { solution: 0, multi: 1 }).solution
+}
--- src/2020/13/13.test.js
+++ src/2020/13/13.test.js
@@ -4,6 +4,9 @@ import input from './input'
 const example = `939
 7,13,x,x,59,x,31,19`
 
+// const example = `939
+// 7,13`
+
 describe('Day 13: ???', () => {
   it('Part 1 - Example', () => {
     expect(part1(example.split('\n'))).toEqual(295)
@@ -11,6 +14,10 @@ describe('Day 13: ???', () => {
   it('Part 1', () => {
     expect(part1(input.split('\n'))).toEqual(2545)
   })
-  it.todo('Part 2 - Example')
-  it.todo('Part 2 - Example')
+  it('Part 2 - Example', () => {
+    expect(part2(example.split('\n'))).toEqual(1068781)
+  })
+  it('Part 2', () => {
+    expect(part2(input.split('\n'))).toEqual(266204454441577)
+  })
 })
