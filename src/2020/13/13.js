5300e8d9f14103820c4e999facb5de52a7097d36
Akallabet <grazianos86@gmail.com>
2020-12-13 09:50:49 +0000

    2020, day 13, part 1

--- src/2020/13/13.js
+++ src/2020/13/13.js
@@ -1,2 +1,12 @@
-export const part1 = (values) => {}
+export const part1 = ([departure, times]) => {
+  const { id, timestamp } = times.split(',').filter(time => time !== 'x').reduce(({ timestamp, id }, time) => {
+    const res = time - (departure % time)
+    const isEarlier = res < timestamp
+    return {
+      timestamp: isEarlier ? res : timestamp,
+      id: isEarlier ? time : id
+    }
+  }, { id: '', timestamp: departure })
+  return id * timestamp
+}
 export const part2 = (values) => {}
--- src/2020/13/13.test.js
+++ src/2020/13/13.test.js
@@ -1,11 +1,16 @@
-// import { part1, part2 } from './13'
-// import input from './input'
+import { part1, part2 } from './13'
+import input from './input'
 
-// const example = ``
+const example = `939
+7,13,x,x,59,x,31,19`
 
 describe('Day 13: ???', () => {
-  it.todo('Part 1 - Example')
-  it.todo('Part 1')
+  it('Part 1 - Example', () => {
+    expect(part1(example.split('\n'))).toEqual(295)
+  })
+  it('Part 1', () => {
+    expect(part1(input.split('\n'))).toEqual(2545)
+  })
   it.todo('Part 2 - Example')
   it.todo('Part 2 - Example')
 })
