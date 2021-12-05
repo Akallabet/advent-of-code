cfd130e4314dd363e772132a59ba5124b68e2a06
Akallabet <grazianos86@gmail.com>
2021-12-05 22:33:13 +0000

    shorten parameters by not passing them as array

--- src/2021/5/5.js
+++ src/2021/5/5.js
@@ -2,40 +2,29 @@ import { filter, map, max, pipe, reduce, split } from 'ramda';
 
 const makeOceanFloor = pipe(
   reduce(
-    ([maxX, maxY], [[xa, ya], [xb, yb]]) => [max(maxX, max(xa, xb)), max(maxY, max(ya, yb))],
+    ([maxX, maxY], [[x1, y1], [x2, y2]]) => [max(maxX, max(x1, x2)), max(maxY, max(y1, y2))],
     [0, 0]
   ),
   ([x, y]) => [...new Array(y + 1)].map(() => [...new Array(x + 1)].map(() => 0))
 );
 
-const drawLine = (fn) => {
-  const drawLineRec = ([[xa, ya], [xb, yb]], floor) => {
-    floor[ya][xa] += 1;
-    if (xa === xb && ya === yb) return floor;
-    return drawLineRec([fn([xa, ya], [xb, yb]), [xb, yb]], floor);
-  };
-  return drawLineRec;
+const drawLine = (x1, y1, x2, y2, floor, increment) => {
+  floor[y1][x1] += 1;
+  if (x1 === x2 && y1 === y2) return floor;
+  return drawLine(...increment(x1, y1, x2, y2), x2, y2, floor, increment);
 };
 
-const verticalLine = ([xa, ya], [_, yb]) => [xa, ya > yb ? ya - 1 : ya + 1];
-const horizontalLine = ([xa, ya], [xb]) => [xa > xb ? xa - 1 : xa + 1, ya];
-const diagonalLine = ([xa, ya], [xb, yb]) => [xa > xb ? xa - 1 : xa + 1, ya > yb ? ya - 1 : ya + 1];
+const verticalLine = (x1, y1, x2, y2) => [x1, y1 > y2 ? y1 - 1 : y1 + 1];
+const horizontalLine = (x1, y1, x2) => [x1 > x2 ? x1 - 1 : x1 + 1, y1];
+const diagonalLine = (x1, y1, x2, y2) => [x1 > x2 ? x1 - 1 : x1 + 1, y1 > y2 ? y1 - 1 : y1 + 1];
 
-const drawLines = (isDiagonal = false) =>
-  reduce((floor, [[xa, ya], [xb, yb]]) => {
-    const fn =
-      (xa === xb && drawLine(verticalLine)) ||
-      (ya === yb && drawLine(horizontalLine)) ||
-      (isDiagonal && Math.abs(xb - xa) === Math.abs(yb - ya) && drawLine(diagonalLine));
-    return fn
-      ? fn(
-          [
-            [xa, ya],
-            [xb, yb],
-          ],
-          floor
-        )
-      : floor;
+const drawLines = (isDiagonal) =>
+  reduce((floor, [[x1, y1], [x2, y2]]) => {
+    const increment =
+      (x1 === x2 && verticalLine) ||
+      (y1 === y2 && horizontalLine) ||
+      (isDiagonal && Math.abs(x2 - x1) === Math.abs(y2 - y1) && diagonalLine);
+    return increment ? drawLine(x1, y1, x2, y2, floor, increment) : floor;
   });
 
 const hydrothermalVenture = (isDiagonal) =>
