93568550af12393775948a51023f83cfa5282811
Akallabet <grazianos86@gmail.com>
2021-12-07 09:37:41 +0000

    2021 - day 7 - part 2

--- src/2021/7/7.js
+++ src/2021/7/7.js
@@ -1,30 +1,40 @@
-import { pipe, reduce, sort, split } from 'ramda';
+import { map, pipe, reduce, sort, split, addIndex } from 'ramda';
+const mapI = addIndex(map);
 
-const calcOptimFuel = (cluster, moveIndex = 0, leastAmoOfFuel) => {
-  if (moveIndex === cluster.length - 1) return leastAmoOfFuel;
-  const move = cluster[moveIndex][0];
+export const summation = pipe(
+  (n) => [...new Array(n)],
+  mapI((_, i) => i + 1),
+  reduce((sum, n) => sum + n, 0)
+);
+
+const calcOptimFuel = (cluster, { move, leastAmOfFuel, hasIncr } = { move: 0 }) => {
+  if (move === cluster[cluster.length - 1][0]) return leastAmOfFuel;
   const fuel = reduce(
-    (fuel, numbers) => fuel + Math.abs(numbers[0] - move) * numbers.length,
+    (fuel, numbers) => {
+      const distance = Math.abs(numbers[0] - move);
+      return fuel + (hasIncr ? summation(distance) : distance) * numbers.length;
+    },
     0,
     cluster
   );
-  return calcOptimFuel(
-    cluster,
-    moveIndex + 1,
-    !leastAmoOfFuel ? fuel : fuel < leastAmoOfFuel ? fuel : leastAmoOfFuel
-  );
+  return calcOptimFuel(cluster, {
+    move: move + 1,
+    leastAmOfFuel: !leastAmOfFuel ? fuel : fuel < leastAmOfFuel ? fuel : leastAmOfFuel,
+    hasIncr,
+  });
 };
 
-export const leastAmountOfFuel = (numbers) =>
-  pipe(
-    sort((a, b) => a - b),
-    reduce((cluster, num) => {
-      if (!cluster[cluster.length - 1]) return [[num]];
-      if (num === cluster[cluster.length - 1][0])
-        return [...cluster.slice(0, cluster.length - 1), [...cluster[cluster.length - 1], num]];
-      return [...cluster, [num]];
-    }, []),
-    calcOptimFuel
-  )(numbers);
+const calcOptimFuelWithIncr = (cluster) => calcOptimFuel(cluster, { move: 0, hasIncr: true });
 
-export const part1 = pipe(split(','), leastAmountOfFuel);
+const makeClusters = pipe(
+  sort((a, b) => a - b),
+  reduce((cluster, num) => {
+    if (!cluster[cluster.length - 1]) return [[num]];
+    if (num === cluster[cluster.length - 1][0])
+      return [...cluster.slice(0, cluster.length - 1), [...cluster[cluster.length - 1], num]];
+    return [...cluster, [num]];
+  }, [])
+);
+
+export const part1 = pipe(split(','), map(Number), makeClusters, calcOptimFuel);
+export const part2 = pipe(split(','), map(Number), makeClusters, calcOptimFuelWithIncr);
--- src/2021/7/7.test.js
+++ src/2021/7/7.test.js
@@ -1,25 +1,37 @@
-import { part1, leastAmountOfFuel } from './7';
+import { part1, part2, summation } from './7';
 import input from './input.js';
 
 const example = '16,1,2,0,4,2,7,1,2,14';
 
 describe('Day 7: The Treachery of Whales', () => {
-  it('2 different close odd numbers', () => {
-    expect(leastAmountOfFuel([5, 1])).toEqual(4);
+  it('Part 1 - 2 different close odd numbers', () => {
+    expect(part1('5,1')).toEqual(4);
   });
-  it('2 different close numbers', () => {
-    expect(leastAmountOfFuel([7, 2])).toEqual(5);
+  it('Part 1 - 2 different close numbers', () => {
+    expect(part1('7,2')).toEqual(5);
   });
-  it('3 different close numbers', () => {
-    expect(leastAmountOfFuel([1, 5, 11])).toEqual(10);
+  it('Part 1 - 3 different close numbers', () => {
+    expect(part1('1,5,11')).toEqual(10);
   });
-  it('3 numbers, 2 equals', () => {
-    expect(leastAmountOfFuel([2, 20, 2])).toEqual(18);
+  it('Part 1 - 3 numbers, 2 equals', () => {
+    expect(part1('2,20,2')).toEqual(18);
   });
   it('Part 1 - Example', () => {
     expect(part1(example)).toEqual(37);
   });
   it('Part 1', () => {
     expect(part1(input)).toEqual(344297);
+  });
+  it('summation', () => {
+    expect(summation(7)).toEqual(28);
+  });
+  it('Part 2 - 2 different close odd numbers', () => {
+    expect(part2('5,1')).toEqual(6);
+  });
+  it('Part 2 - Example', () => {
+    expect(part2(example)).toEqual(168);
+  });
+  it('Part 2', () => {
+    expect(part2(input)).toEqual(97164301);
   });
 });
