1d557ccc5898e1707261ab1ed07442e4af785167
Akallabet <grazianos86@gmail.com>
2021-12-08 08:34:09 +0000

    wip part2

--- src/2021/8/8.js
+++ src/2021/8/8.js
@@ -1,8 +1,9 @@
-import { map, pipe, reduce, split } from 'ramda';
+import { indexOf, join, map, pipe, reduce, sort, split, sum, tap } from 'ramda';
+
+const extractInput = pipe(split('\n'), map(split(' | ')));
 
 export const part1 = pipe(
-  split('\n'),
-  map(split(' | ')),
+  extractInput,
   map(map(split(' '))),
   reduce(
     (instances, [, output]) =>
@@ -13,4 +14,29 @@ export const part1 = pipe(
       ).length,
     0
   )
+);
+const digitsOrder = ['d', 'e', 'a', 'f', 'g', 'b', 'c'];
+const digitsMapping = [
+  'ab',
+  'dafgc',
+  'dafbc',
+  'eafb',
+  'defbc',
+  'defgbc',
+  'dab',
+  'deafgbc',
+  'deafbc',
+];
+const sortDigits = sort((a, b) => digitsOrder.indexOf(a) - digitsOrder.indexOf(b));
+const decode = pipe(
+  map(sortDigits),
+  map((d) => digitsMapping.indexOf(d) + 1),
+  join(''),
+  Number
+);
+
+export const part2 = pipe(
+  extractInput,
+  map(([segments, output]) => decode(output)),
+  sum
 );
--- src/2021/8/8.test.js
+++ src/2021/8/8.test.js
@@ -1,6 +1,7 @@
-import { part1 } from './8';
+import { part1, part2 } from './8';
 import input from './input.js';
 
+const easyExample = `acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf`;
 const example = `be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
 edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
 fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
@@ -17,6 +18,9 @@ describe('Day 8: Seven Segment Search', () => {
     expect(part1(example)).toEqual(26);
   });
   it('Part 1 - input', () => {
-    expect(part1(input)).toEqual(26);
+    expect(part1(input)).toEqual(456);
+  });
+  it('Part 2 - easy example', () => {
+    expect(part2(easyExample)).toEqual(5353);
   });
 });
