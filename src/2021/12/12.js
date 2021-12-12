24a208fe012244891d8fab6b3a0e05b056bad95b
Akallabet <grazianos86@gmail.com>
2021-12-12 19:47:00 +0000

    2021 day 12 - part 1

--- src/2021/12/12.js
+++ src/2021/12/12.js
@@ -1,5 +1,6 @@
-import { map, pipe, reduce, split } from 'ramda';
+import { includes, map, pipe, reduce, split } from 'ramda';
 
+export const isBigCave = (cave) => /[ABCDEFGHIKLMNPQRSTUVWYXZ]/.test(cave);
 export const parseInput = pipe(split('\n'), map(split('-')));
 export const buildGraph = pipe(
   reduce(
@@ -8,4 +9,17 @@ export const buildGraph = pipe(
   )
 );
 
-export const part1 = () => {};
+export const findPaths = (graph, start, end, current, visited = []) =>
+  current === end
+    ? 1
+    : includes(current, visited) && !isBigCave(current)
+    ? 0
+    : graph[current].reduce(
+        (nOfPaths, adjacent) =>
+          nOfPaths + findPaths(graph, start, end, adjacent, [...visited, current]),
+        0
+      );
+
+const findDistinctPathsForTwoNodes = (start, end) => (graph) => findPaths(graph, start, end, start);
+
+export const part1 = pipe(parseInput, buildGraph, findDistinctPathsForTwoNodes('start', 'end'));
--- src/2021/12/12.test.js
+++ src/2021/12/12.test.js
@@ -1,5 +1,5 @@
-import { part1, parseInput, buildGraph } from './12';
-// import input from './input';
+import { part1, parseInput, buildGraph, findPaths, isBigCave } from './12';
+import input from './input';
 
 const example = `start-A
 start-b
@@ -9,7 +9,7 @@ b-d
 A-end
 b-end`;
 
-describe('Day 12 - Passage Pathing', () => {
+describe('Utils', () => {
   it('Parse input', () => {
     expect(parseInput(example)).toEqual([
       ['start', 'A'],
@@ -21,6 +21,16 @@ describe('Day 12 - Passage Pathing', () => {
       ['b', 'end'],
     ]);
   });
+  it('big caves', () => {
+    expect(isBigCave('A')).toBeTruthy();
+    expect(isBigCave('AB')).toBeTruthy();
+    expect(isBigCave('b')).toBeFalsy();
+    expect(isBigCave('dc')).toBeFalsy();
+    expect(isBigCave('start')).toBeFalsy();
+  });
+});
+
+describe('Graph', () => {
   it('Build graph', () => {
     expect(buildGraph(parseInput(example))).toEqual({
       start: ['A', 'b'],
@@ -31,7 +41,27 @@ describe('Day 12 - Passage Pathing', () => {
       end: ['A', 'b'],
     });
   });
-  it('Part 1 - Simple example', () => {
-    expect(part1(example)).toEqual(10);
+
+  it('Find path - simple', () => {
+    const graph = buildGraph(parseInput('start-a\na-b\nb-end'));
+    expect(findPaths(graph, 'start', 'end', 'start')).toEqual(1);
+  });
+
+  it('Find path - big caves', () => {
+    const bigCaves = `start-A
+A-b
+A-end`;
+    const graph = buildGraph(parseInput(bigCaves));
+    expect(findPaths(graph, 'start', 'end', 'start')).toEqual(2);
+  });
+  it('Find paths - Example', () => {
+    const graph = buildGraph(parseInput(example));
+    expect(findPaths(graph, 'start', 'end', 'start')).toEqual(10);
+  });
+});
+
+describe('Day 12 - Passage Pathing', () => {
+  it('Part 1', () => {
+    expect(part1(input)).toEqual(3713);
   });
 });
