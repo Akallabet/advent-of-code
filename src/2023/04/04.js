b2e82113c1e3ecda4e35a55a681b69456d10e1f8
Graziano Statello <grazianos86@gmail.com>
2023-12-05 00:02:16 +0100

    2023 day 4 part 2

--- src/2023/04/04.js
+++ src/2023/04/04.js
@@ -1,5 +1,5 @@
-export function scratchCards (input) {
-  const values = input.split('\n').filter(Boolean).map(line => line.split(': ')).map(([card, numbers]) => {
+function getValues (input) {
+  return input.split('\n').filter(Boolean).map(line => line.split(': ')).map(([card, numbers]) => {
     const [winningNumbers, ownNumbers] = numbers.split(' | ').map(set => set.split(' ').filter(Boolean).map(Number))
     return {
       card,
@@ -7,9 +7,60 @@ export function scratchCards (input) {
       ownNumbers
     }
   })
-  return values
-    .map(({ winningNumbers, ownNumbers }) => winningNumbers.filter(num => ownNumbers.includes(num)))
-    .filter(nums => nums.length > 0)
-    .map(nums => nums.slice(1).reduce((acc) => acc * 2, 1))
+}
+
+function calcCardScore (nums) {
+  return nums.slice(1).reduce((acc) => acc * 2, 1)
+}
+
+function calcScore (cards) {
+  return cards
+    .map(calcCardScore)
     .reduce((acc, val) => acc + val, 0)
+}
+
+function calcOwnWinningNumbers ({ winningNumbers, ownNumbers }) {
+  return winningNumbers.filter(num => ownNumbers.includes(num))
+}
+
+function addOwnWinningNumbers ({ card, winningNumbers, ownNumbers }) {
+  return {
+    card,
+    ownWinningNumbers: calcOwnWinningNumbers({ winningNumbers, ownNumbers }),
+    ownNumbers
+  }
+}
+
+export function scratchCards (input) {
+  const winningCards = getValues(input)
+    .map(addOwnWinningNumbers)
+    .filter(({ ownWinningNumbers }) => ownWinningNumbers.length > 0)
+    .map(({ ownWinningNumbers }) => ownWinningNumbers)
+
+  return calcScore(winningCards)
+}
+
+export function scratchCardsPart2 (input) {
+  const values = getValues(input).map(({ card, winningNumbers, ownNumbers }) => {
+    const ownWinningNumbers = calcOwnWinningNumbers({ winningNumbers, ownNumbers })
+    return {
+      card: Number(card.split(' ')[1]),
+      score: ownWinningNumbers.length > 0 ? calcCardScore(ownWinningNumbers) : 0,
+      wins: ownWinningNumbers.length,
+      instances: 1
+    }
+  })
+  values.forEach((card, i, cards) => {
+    if (card.wins > 0) {
+      const from = i + 1
+      const to = i + card.wins
+      // console.log({ card, from, to })
+      for (let x = from; x <= to && x < cards.length; x++) {
+        cards[x].instances += card.instances
+      }
+    }
+  })
+  // console.log(values)
+
+  return values.reduce((acc, { instances }) => acc + instances, 0)
 }
--- src/2023/04/04.test.js
+++ src/2023/04/04.test.js
@@ -2,7 +2,7 @@ import test from 'node:test'
 import { strict as assert } from 'node:assert'
 import { readFile } from 'node:fs/promises'
 import path from 'node:path'
-import { scratchCards } from './04.js'
+import { scratchCards, scratchCardsPart2 } from './04.js'
 
 const inputValues = () => readFile(path.resolve('./src/2023/04/input.txt'), 'utf8')
 
@@ -13,12 +13,22 @@ Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
 Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
 
-// test('Scratch cards - Part 1 - test values', () => {
-//   const result = scratchCards(testValues)
-//   assert.strictEqual(result, 13)
-// })
+test('Scratch cards - Part 1 - test values', () => {
+  const result = scratchCards(testValues)
+  assert.strictEqual(result, 13)
+})
 
 test('Scratch cards - Part 1', async () => {
   const result = scratchCards(await inputValues())
-  assert.strictEqual(result, 13)
+  assert.strictEqual(result, 33950)
+})
+
+test('Scratch cards - Part 2 - test values', () => {
+  const result = scratchCardsPart2(testValues)
+  assert.strictEqual(result, 30)
+})
+
+test('Scratch cards - Part 2', async () => {
+  const result = scratchCardsPart2(await inputValues())
+  assert.strictEqual(result, 14814534)
 })
