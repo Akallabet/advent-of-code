abd7dc14c80d9b2f189a2ef0517a9c970a94b48e
Graziano Statello <grazianos86@gmail.com>
2023-12-10 07:57:58 +0100

    2023 day 8 part 2

--- src/2023/08/08.js
+++ src/2023/08/08.js
@@ -1,31 +1,60 @@
-export function hauntedWastelands (input) {
+function getValues (input) {
   const [instructionsLine, ...nodeLines] = input.split('\n').filter(Boolean)
   const instructions = instructionsLine.split('').map(i => i === 'L' ? 0 : 1)
 
-  const root = 'AAA'
-  const leef = 'ZZZ'
-
-  const nodes = nodeLines.reduce((acc, line) => {
+  const nodes = []
+  const path = nodeLines.reduce((acc, line) => {
     const [el, value] = line.split(' = ')
+    nodes.push(el)
     const connections = value.replace(/[()]/g, '').split(', ')
     acc[el] = connections
     return acc
   }, {})
 
+  return { instructions, path, nodes }
+}
+
+function findLeaf ({ instructions, nextNodeKey, currentNode, isLeaf = () => {}, path }) {
   let steps = 0
-  let currentNode = nodes[root]
-  let nextNodeKey = ''
 
   while (true) {
     for (const direction of instructions) {
       nextNodeKey = currentNode[direction]
-      currentNode = nodes[nextNodeKey]
+      currentNode = path[nextNodeKey]
       steps++
-      if (nextNodeKey === leef) break
+      if (isLeaf(nextNodeKey)) break
     }
-    if (nextNodeKey === leef) break
-    if (steps > 1000000) break
+      if (isLeaf(nextNodeKey)) break
   }
 
   return steps
+}
+
+export function hauntedWastelands (input) {
+  const { instructions, path } = getValues(input)
+
+  const root = 'AAA'
+
+  const currentNode = path[root]
+  const nextNodeKey = ''
+
+  const steps = findLeaf({ instructions, nextNodeKey, currentNode, isLeaf: (nodeKey)=> nodeKey === 'ZZZ', path })
+  return steps
+}
+
+const lcm = (...arr) => {
+  const gcd = (x, y) => (!y ? x : gcd(y, x % y));
+  const _lcm = (x, y) => (x * y) / gcd(x, y);
+  return [...arr].reduce((a, b) => _lcm(a, b));
+};
+
+export function hauntedWastelandsPart2 (input) {
+  const { instructions, path, nodes } = getValues(input)
+  const roots = nodes.filter(node => node.endsWith('A'))
+
+  const currentNodes = roots.map(root => path[root])
+
+  const steps = roots.map(root => findLeaf({ instructions, nextNodeKey: '', currentNode: path[root], isLeaf: (nodeKey) => nodeKey.endsWith('Z'), path }))
+
+  return lcm(...steps)
 }
--- src/2023/08/08.test.js
+++ src/2023/08/08.test.js
@@ -2,7 +2,7 @@ import test from 'node:test'
 import { strict as assert } from 'node:assert'
 import { readFile } from 'node:fs/promises'
 import path from 'node:path'
-import { hauntedWastelands } from './08.js'
+import { hauntedWastelands, hauntedWastelandsPart2 } from './08.js'
 
 const inputValues = () => readFile(path.resolve('./src/2023/08/input.txt'), 'utf8')
 
@@ -12,12 +12,22 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)`
 
-// test('Camel cards- Part 1 - test values', () => {
-//   const result = hauntedWastelands(testValues)
-//   assert.strictEqual(result, 6)
-// })
+test('Camel cards- Part 1 - test values', () => {
+  const result = hauntedWastelands(testValues)
+  assert.strictEqual(result, 6)
+})
 
 test('Camel cards- Part 1', async () => {
   const result = hauntedWastelands(await inputValues())
+  assert.strictEqual(result, 19199)
+})
+
+test('Camel cards- Part 2 - test values', () => {
+  const result = hauntedWastelandsPart2(testValues)
   assert.strictEqual(result, 6)
+})
+
+test('Camel cards- Part 2', async () => {
+  const result = hauntedWastelandsPart2(await inputValues())
+  assert.strictEqual(result, 13663968099527)
 })
